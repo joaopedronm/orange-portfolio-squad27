a7758477ae6ef6a716209ec831288e4f6075f7ae
Estefania Carrer <estefania.carrer@hotmail.com>
2024-01-31 23:41:37 -0300

    Adicionando validacoes dos campos de preenchimento

--- backend/src/db/conn.js
+++ backend/src/db/conn.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose')
 
 async function main() {
-    const uri = "mongodb://root:example@localhost:27017/mydatabase?authSource=admin";
-    await mongoose.connect(uri);
+    await mongoose.connect('mongodb://127.0.0.1:27017/orangeportfolio')
     console.log("Conectou ao Mongoose!")
 }
 
--- frontend/pagina-de-cadastro/cadastroUser.js
+++ frontend/pagina-de-cadastro/cadastroUser.js
@@ -1,4 +1,4 @@
-const url = "http://localhost:3000/user/register"
+const url = "http://localhost:3000/user/register";
 
 function fazPost(url, corpo) {
   return fetch(url, {
@@ -9,31 +9,44 @@ function fazPost(url, corpo) {
       "Access-Control-Allow-Headers": "*",
       "Content-Type": "application/json",
     },
-  }).then((response) => {
-    if (response.ok) {
-      return response.json();
-    }
-    throw new Error();
-  }).then((response) => {
-    console.log(response);
-    alert("Usuário cadastrado com sucesso");
-  }).catch((err) => {
-    alert("Erro no cadastro");
-  });
+  })
+    .then((response) => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error();
+    })
+    .then((response) => {
+      console.log(response);
+      alert("Usuário cadastrado com sucesso!");
+      window.location.href = "meu-portifolio.html"; //verificar se é dessa forma
+    })
+    .catch((err) => {
+      alert("E-mail já cadastrado!");
+    });
 }
 
 const cadastroButton = document.getElementById("botao-cadastrar");
 cadastroButton.addEventListener("click", (event) => cadastrarUsuario(event));
 
 function cadastrarUsuario(event) {
-  // URL da sua rota de registro de usuário
-  event.preventDefault()
+  event.preventDefault();
+
   const nome = document.getElementById("nome").value;
   const sobrenome = document.getElementById("sobrenome").value;
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirmar-password").value;
 
+  if (!nome || !sobrenome || !email || !password || !confirmPassword) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
+
+  if (password !== confirmPassword) {
+    alert("A senha e a confirmação de senha não correspondem.");
+    return;
+  }
 
   const corpo = {
     nome: nome,
