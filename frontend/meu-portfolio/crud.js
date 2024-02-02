009e92e14681570c6eda6a55c8516b77ecf185ca
Estefania Carrer <estefania.carrer@hotmail.com>
2024-02-01 21:42:15 -0300

    Ajuste no upload da imagem

--- frontend/meu-portfolio/crud.js
+++ frontend/meu-portfolio/crud.js
@@ -1,51 +1,48 @@
-const url = "http://localhost:3000/projeto/create"
+const url = "http://localhost:3000/projeto/create";
 
 function criaProjeto(url, projeto) {
-  const token = localStorage.getItem("token"); // Obter o token JWT do localStorage
+  const token = localStorage.getItem("token");
 
   if (token) {
-  return fetch(url, {
-    method: "POST",
-    mode: "cors",
-    body: JSON.stringify(projeto),
-    headers: {
-      "Access-Control-Allow-Headers": "*",
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${token}`
-    },
-  }).then(async (response) => {
-    if (response.ok) {
-      alert("Projeto adicionado com sucesso!");
-      console.log(projeto);
-    } else {
-      const error = await response.json();
-      alert(error.message);
-    }
-  });
-}
+    return fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: projeto,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then(async (response) => {
+      if (response.ok) {
+        alert("Projeto adicionado com sucesso!");
+        console.log(projeto);
+      } else {
+        const error = await response.json();
+        alert(error.message);
+      }
+    });
+  }
 }
 
 const adicionarProjetoButton = document.getElementById("adicionar-projeto");
-adicionarProjetoButton.addEventListener("click", (event) => cadastrarProjeto(event));
+adicionarProjetoButton.addEventListener("click", (event) =>
+  cadastrarProjeto(event)
+);
 
 function cadastrarProjeto(event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const img = document.getElementById('img-input')
-  const titulo = document.getElementById('titulo').value;
-  const tags = document.getElementById('tags-form').value;
-  const link = document.getElementById('link').value;
-  const descricao = document.getElementById('descricao').value;
+  const img = document.getElementById("file-input").files[0];
+  const titulo = document.getElementById("titulo").value;
+  const tags = document.getElementById("tags-form").value;
+  const link = document.getElementById("link").value;
+  const descricao = document.getElementById("descricao").value;
 
-  const projeto = {
-    img: img,
-    titulo: titulo,
-    tags: tags,
-    link: link,
-    descricao: descricao
-  }
+  const projeto = new FormData();
+  projeto.append("imagem", img);
+  projeto.append("titulo", titulo);
+  projeto.append("tags", tags);
+  projeto.append("link", link);
+  projeto.append("descricao", descricao);
 
   criaProjeto(url, projeto);
-
 }
-
