0d344ebb92997dfa69b34bdc428d2c8a40339752
MRThome <matheusrthome@hotmail.com>
2024-02-03 01:56:00 -0300

    feat: fetch para retornar todos os projetos armazenados

--- frontend/home/home.js
+++ frontend/home/home.js
@@ -1,3 +1,24 @@
+const homeModal = document.getElementById('home-modal')
+const projetosContainer = document.getElementById('projetos-container')
+
+fetch('http://localhost:3000/projeto/descobrir', {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('token')}`,
+  },
+})
+  .then(res => res.json())
+  .then(projetos => {
+    projetos.projeto.forEach(projeto => {
+      console.log(projeto);
+      /* console.log(projeto.titulo);
+      console.log(projeto.tags);
+      console.log(projeto.link);
+      console.log(projeto.descricao);
+      console.log(projeto.imagem); */
+      exibirProjeto(projeto);
+    });
+  });
+
 
 // const openModalBtns = document.getElementsByClassName('.projeto-imagem')
 const openModalBtns = document.getElementsByClassName('projeto-imagem')
@@ -6,20 +27,20 @@ const closeModalBtn = document.getElementById('closeModalBtn')
 const modal = document.getElementById('home-modal')
 
 for (const btn of openModalBtns) {
-    btn.addEventListener('click', () => {
-      modal.style.display = 'block';
-    });
-  }
+  btn.addEventListener('click', () => {
+    modal.style.display = 'block';
+  });
+}
 
 closeModalBtn.addEventListener('click', () => {
-    modal.style.display = 'none'
+  modal.style.display = 'none'
 })
 
 // A função abaixo fecha o modal caso o usuário clique fora dele
 window.addEventListener('click', function (e) {
-    if (e.target == modal) {
-      modal.style.display = 'none';
-    }
+  if (e.target == modal) {
+    modal.style.display = 'none';
+  }
 });
 
 // A FUNÇÃO ABAIXO É RESPONSÁVEL POR ATIVAR/DESATIVAR O MENU HAMBÚRGUER
@@ -42,14 +63,14 @@ class MobileNavbar {
   }
 
   init() {
-    if(this.mobileMenu) {
+    if (this.mobileMenu) {
       this.addClickEvent()
     }
     return this
   }
 }
 
-const mobileNavBar = new MobileNavbar (
+const mobileNavBar = new MobileNavbar(
   ".mobile-menu",
   "#menu-links",
   "#menu-links li",
