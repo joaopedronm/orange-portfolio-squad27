6b3a24ffcdbda19f0c7253a8e6a8dd31c86b234e
MRThome <matheusrthome@hotmail.com>
2024-02-04 19:05:35 -0300

    fix: ajustes no login com o google

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController")
+const testeController = require("../controllers/testeController")
 
 //const verifyToken = require("../util/check-token");
 
@@ -9,6 +10,7 @@ router.get('/auth/google', userController.googleLogin);
 router.get('/auth/google/callback', userController.googleLoginCallback);
 router.get("/checkuser", userController.checkUser);
 router.get("/:id", userController.getUserById);
+router.get("/teste", testeController.getTeste);
 //router.patch("/edit/:id", verifyToken, userController.editUser);
 
 module.exports = router;
