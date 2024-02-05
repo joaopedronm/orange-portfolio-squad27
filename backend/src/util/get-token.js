aaf371383d5d7c3cfcc02e11cf78fb7faa956656
MRThome <matheusrthome@hotmail.com>
2024-02-05 16:23:58 -0300

    Ajustes da branch para deploy

--- backend/src/util/get-token.js
+++
@@ -1,9 +0,0 @@
-// get token from headers
-const getToken = (req) => {
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-  
-    return token;
-  };
-  
-  module.exports = getToken;
